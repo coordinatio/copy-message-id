8433e2be9af670b94fc4f9c085cbd25ebd1f818c
Joshua Kahn <joshkahn@umich.edu>
2026-05-24 14:00:32 -0400

    Activate button on shortcut, cleanup options pane

--- background.js
+++ background.js
@@ -1,45 +1,7 @@
 browser.commands.onCommand.addListener(async (command) => {
-	if (command !== "copy-message-id") {
-		return;
-	}
-
+	if (command !== "copy-message-id") return;
 	try {
-		let tabs = await browser.tabs.query({active: true, currentWindow: true});
-		if (tabs.length != 1) {
-			throw new Error("Expected a single selected tab (got " + tabs.length + ")");
-		}
-
-		let tabId = tabs[0].id;
-		let message = await browser.messageDisplay.getDisplayedMessage(tabId);
-		if (!message) {
-			throw new Error("No message selected");
-		}
-
-		var options = {
-			prefix: "",
-			suffix: "",
-			copyBrackets: false,
-			urlEncode: false,
-		};
-		let config = await browser.storage.local.get("copyID");
-		if (config.copyID) {
-			options = config.copyID;
-		}
-
-		let parts = await browser.messages.getFull(message.id);
-		let message_id = parts.headers["message-id"][0];
-
-		if (!options.copyBrackets &&
-				message_id[0] == '<' &&
-				message_id[message_id.length - 1] == '>') {
-			message_id = message_id.slice(1, -1);
-		}
-		if (options.urlEncode) {
-			message_id = encodeURIComponent(message_id);
-		}
-
-		let s = options.prefix + message_id + options.suffix;
-		await navigator.clipboard.writeText(s);
+		await browser.messageDisplayAction.openPopup();
 	} catch (error) {
 		console.error("Copy Message ID shortcut failed: " + error.message);
 	}
